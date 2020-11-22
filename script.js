c7f8346df4b08658de3b5ba4eb9f580b82144391
Elizabeth Ponce <ponce.emc@gmail.com>
2020-11-22 13:22:12 -0800

    timer and buttons now working

--- script.js
+++ script.js
@@ -1,14 +1,50 @@
-var slideIndex = 0;
+//carousel js, css and html   adapted from https://markus.oberlehner.net/ and w3schools
+var slideIndex = 1;
 showSlides();
+var myTimer;
+var slideshowContainer;
 
-function showSlides() {
+window.addEventListener("load", function(){
+  showSlides(slideIndex);
+  myTimes = setInterval(function(){plusSlides(1)}, 4000);
+})
+
+showSlides(slideIndex);
+
+function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+      slides[i].style.display = "none";
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+  dots[slideIndex-1].className += " active";
+}
+
+//this is the prev and back arrows
+function plusSlides(n) {
+  //showSlides(slideIndex += n);
+  clearInterval(myTimer);
+  if(n < 0){
+    showSlides(slideIndex -= 1);
+  }else{
+    showSlides(slideIndex += 1);
+  }
+  if (n === -1){
+    myTimer = setInterval(function(){plusSlides(n + 2)}, 4000);
+  }else{
+    myTimer = setInterval(function(){plusSlides(n + 1)}, 4000);
+  }
+}
+
+function currentSlide(n) {
+  clearInterval(myTimer);
+  myTimer = setInterval(function(){plusSlides(n+1)}, 4000);
+  showSlides(slideIndex = n);
 }
